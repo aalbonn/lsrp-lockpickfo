c38e6dfe80904d68a034c067a61e46be90fd4134
aalbonn <96874392+aalbonn@users.noreply.github.com>
2022-01-24 03:13:30 +0300

    Add files via upload

---
+++ lsrp-lockpickfo/__resource.lua
@@ -0,0 +1,10 @@
+client_script 'client/client.lua'
+
+ui_page 'client/html/index.html'
+
+files({
+    'client/html/index.html',
+    'client/html/js/*.js',
+    'client/html/img/*.png',
+    'client/html/css/*.css',
+})
